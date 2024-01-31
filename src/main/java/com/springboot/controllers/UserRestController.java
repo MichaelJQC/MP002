40fe1d0a967107d631608c4712c0c504534512b4
MichaelJQC <michael.chavezjq@gmail.com>
2024-01-31 12:02:30 -0500

    RequestMapping annotation

--- src/main/java/com/springboot/controllers/UserRestController.java
+++ src/main/java/com/springboot/controllers/UserRestController.java
@@ -1,13 +1,15 @@
 package com.springboot.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
-    @GetMapping("/details/v2")
+    @GetMapping("/details")
     public Map<String , Object> details() {
         Map<String , Object> body = new HashMap<>();
         body.put("title" , "Hola mundo Sring Boot");
